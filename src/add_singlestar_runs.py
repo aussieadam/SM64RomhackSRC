11acf685193e7511b67e6c57cfa97007d8df346c
Adam Ferguson <afergus6@masonlive.gmu.edu>
2023-05-23 13:25:01 -0400

    added optimization for user check

--- src/add_singlestar_runs.py
+++ src/add_singlestar_runs.py
@@ -5,8 +5,7 @@ import os
 from dotenv import load_dotenv
 import pandas as pd
 
-
-#need to run get_romhacks.py first, you could couple them together, but i prefer separating so i can use hacks.json elsewhere without having to run a long method everytime
+# need to run get_romhacks.py first, you could couple them together, but i prefer separating so i can use hacks.json elsewhere without having to run a long method everytime
 
 parser = argparse.ArgumentParser(description="Add Single Star runs parser")
 
@@ -98,7 +97,7 @@ def get_game_level_star(game_name, level_name, star_name):
             elif 'Star Speedruns' in hack['categories']:
                 cat_id = hack['categories']['Star Speedruns']
             record = {}
-            #this is really stupid, SRC wants a list of all variables, even if they are unused, so we have to go through and set them all, but also make sure we set the right variable
+            # this is really stupid, SRC wants a list of all variables, even if they are unused, so we have to go through and set them all, but also make sure we set the right variable
             for var_id, vars in hack['variables'].items():
                 if var_id.lower() == level_name.lower():
                     for val_id, val in vars['choices'].items():
@@ -106,7 +105,7 @@ def get_game_level_star(game_name, level_name, star_name):
                             record['final_record'] = [hack['id'], vars['id'], vars['level_id'], val_id, cat_id]
                 else:
                     for val_id, val in vars['choices'].items():
-                        record[vars['id']] = {"type": "pre-defined","value": val_id}
+                        record[vars['id']] = {"type": "pre-defined", "value": val_id}
                         break
             stars_list.append(record)
             break
@@ -120,6 +119,7 @@ if __name__ == '__main__':
     multiple_matching_stars = []
     missing_urls = []
     missing_users = []
+    users = {}
     for index, run in read_file.iterrows():
         # only want to upload video proof
         hack_name = run['Rom Hack Name']
@@ -128,24 +128,25 @@ if __name__ == '__main__':
         user_name = run['Runner']
         if run['Type of Proof'] != 'Video':
             continue
-        #check that the course name is filled out, ideally it always is, but we can search for stars without it
-        if run['Course Name'] is not None and run['Course Name'] not in ['','nan']:
+        # check that the course name is filled out, ideally it always is, but we can search for stars without it
+        if run['Course Name'] is not None and run['Course Name'] not in ['', 'nan']:
             game_res = get_game_level_star(hack_name, course_name, star_name)
         else:
             game_res = get_game_star(hack_name, star_name)
-        #did not return a game
+        # did not return a game
         if len(game_res) == 0 or game_res is None:
-            no_matching_stars.append([hack_name,course_name, star_name,"missing game from SRC or name doesn't match SRC"])
+            no_matching_stars.append(
+                [hack_name, course_name, star_name, "missing game from SRC or name doesn't match SRC"])
             continue
-        #multiple stars with same name
+        # multiple stars with same name
         if len(game_res) > 1:
             multiple_matching_stars.append([hack_name, star_name])
             continue
 
         game_res = game_res[0]
         if 'final_record' not in game_res:
-            #course or star not in game_res
-            no_matching_stars.append([hack_name,course_name, star_name,"Course Name or Star Name not found"])
+            # course or star not in game_res
+            no_matching_stars.append([hack_name, course_name, star_name, "Course Name or Star Name not found"])
             continue
         final_res = game_res['final_record']
         game_id = final_res[0]
@@ -156,40 +157,46 @@ if __name__ == '__main__':
         del game_res['final_record']
 
         if cat_id is None:
-            #hack missing single stars, or the name of single star category isn't in get_game_level_star body
-            no_matching_stars.append([hack_name,course_name, star_name,"Hack is missing a single star category or get_game_level_star doesn't have the name of the category in SRC"])
+            # hack missing single stars, or the name of single star category isn't in get_game_level_star body
+            no_matching_stars.append([hack_name, course_name, star_name,
+                                      "Hack is missing a single star category or get_game_level_star doesn't have the name of the category in SRC"])
             continue
 
-        #people without a SRC can still be added
-        if user_name in ['Maha Maha','Cryogeon','Madghostek','WWM Resident']:
+        # people without a SRC can still be added
+        if user_name in ['Maha Maha', 'Cryogeon', 'Madghostek', 'WWM Resident']:
             user_id = user_name.replace(" ", "")
+        # try not to do an extra SRC call if we've already gotten back this persons user_id
+        elif user_name in users:
+            user_id = users[user_name]
         else:
             user_url = srcHelper.get_user(user_name)
             user_id = srcHelper.request_src(user_url)['data']['id']
+            users[user_name] = user_id
 
         if user_id is None:
             missing_users.append(user_name)
             continue
 
-        #check the proof if youtube vid, if not, check if there's a backup video
+        # check the proof if youtube vid, if not, check if there's a backup video
         if run['Proof'] is not None and ('youtube' in run['Proof'] or 'youtu.be' in run['Proof']):
             run_url = run['Proof']
-        elif run['Backup Video'] is not None and run['Backup Video'] not in ['','nan'] and ('youtube' in run['Backup Video'] or 'youtu.be' in run['Backup Video']):
+        elif run['Backup Video'] is not None and run['Backup Video'] not in ['', 'nan'] and (
+                'youtube' in run['Backup Video'] or 'youtu.be' in run['Backup Video']):
             run_url = run['Backup Video']
         else:
-            missing_urls.append([hack_name,course_name,star_name,user_name])
+            missing_urls.append([hack_name, course_name, star_name, user_name])
             continue
 
         run_time = get_secs(str(run['Time']))
         run_date = run['Date'].strftime('%Y-%m-%d')
         run_json = {'game_id': game_id, 'cat_id': cat_id, 'user_id': user_id, 'run_time': run_time,
                     'run_date': run_date, 'run_url': run_url, 'level_id': level_id, 'star_id': star_id,
-                    'var_id': var_id,'stupid_vars':game_res}
+                    'var_id': var_id, 'stupid_vars': game_res}
 
         # check this run isn't already on SRC
         runs_by_user_game_cat_url = srcHelper.get_runs_by_user_game_cat(user_id, game_id, cat_id)
         found_run = False
-        #if the user has a lot of runs for this game it will paginate the results, max is 200, it'd be very rare, but this could catch an edge case
+        # if the user has a lot of runs for this game it will paginate the results, max is 200, it'd be very rare, but this could catch an edge case
         while runs_by_user_game_cat_url is not None:
             runs_by_user_game_cat = srcHelper.request_src(runs_by_user_game_cat_url)
             for urn in runs_by_user_game_cat['data']:
@@ -210,8 +217,6 @@ if __name__ == '__main__':
         if not found_run:
             run_data.append(run_json)
 
-
-
     for run in run_data:
         add_run_request_body = srcHelper.get_singlestar_run_body(run['user_id'], run['cat_id'], run['run_time'],
                                                                  run['run_date'],
@@ -225,15 +230,12 @@ if __name__ == '__main__':
         else:
             print(run)
 
-
-
-
-
     for multiple_match in multiple_matching_stars:
         print(f'{multiple_match[0]} has multiple stars named : {multiple_match[1]}')
 
     for non_matching in no_matching_stars:
-        print(f'{non_matching[0]} does not exist, has no coursed named {non_matching[1]} , or has no star named : {non_matching[2]}, my guess is: {non_matching[3]}')
+        print(
+            f'{non_matching[0]} does not exist, has no coursed named {non_matching[1]} , or has no star named : {non_matching[2]}, my guess is: {non_matching[3]}')
 
     for run in missing_urls:
         print(f'{run[0]}:{run[1]}:{run[2]}:  for {run[3]} missing youtube video proof')
